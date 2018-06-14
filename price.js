09160cfe2d26546334e9e93efb05fa5cb819156b
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-06-15 08:54:46 +0900

    Update price.js

--- price.js
+++ price.js
@@ -2,4 +2,6 @@ const CoinMarketCap = require('coinmarketcap-api');
  
 const client = new CoinMarketCap();
 
-client.getTicker({currency: ''}).then(console.log).catch(console.error);
+//client.getListings().then(console.log).catch(console.error)
+
+client.getTicker({id : 5}).then(console.log).catch(console.error);
