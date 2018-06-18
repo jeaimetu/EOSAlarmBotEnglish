84bfb974f3dad764aaafc059df7ac386e4d1ea4a
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-06-18 16:39:47 +0900

    Update bot.js
    delete unused codes

--- bot.js
+++ bot.js
@@ -82,35 +82,6 @@ function checkData(ctx){
   return true;
 }
 
-function setEosBalance(ctx){
-//var setEosBalance = (ctx, callback) => {
-  //get EOS balance
-
-
-  var balance = 0;
-  var eos = -1;
-  
-  //check result.
-  balance.then(function(balanceData){
-    console.log("setEosBalance", balanceData, balanceData.message);
-    //if NOT, then set -1
-    //if OK, then set the number after calculation
-    if(balanceData.message.toString() == "NOTOK"){
-      eos = -1;
-    }else{
-      eos = balanceData.result / 1000000000000000000;
-      //update the EOS data to DB
-      //saveData(ctx, eos);
-    }
-    ctx.session.eos = eos;  
-  }, function(err){
-    eos = -1;
-    ctx.session.eos = eos; 
-    console.log("setEosBalance error", err);
-  });
-
-}
-
 function loadData(ctx, cb){
  MongoClient.connect(url, function(err, db) {
  var dbo = db.db("heroku_9472rtd6");
@@ -183,15 +154,6 @@ bot.use(session())
 bot.use(Telegraf.log())
 
 
-bot.on('', msg => {
-
-    const id = msg.from.id;
- console.log("Bot On is called");
-
-    // Ask user name
-  //
-
-});
 
 module.exports.sendAlarm = function(account, msg){
  //get chatid
@@ -213,19 +175,9 @@ module.exports.sendAlarm = function(account, msg){
 }
 
 
-  
-
-
-//set message check by 1min
-//setInterval(function(){checkAlarm();},1000);
-//bot.telegram.sendMessage(548888468, "test messsage");
 
 bot.start((ctx) => {
-  //parameter parsing
- 
 
-  //this cause cyclic dependency detected error
-//saveData(ctx);
   //save etc values
   ctx.session.telegram = ctx.message.chat.username;
   ctx.session.language = ctx.message.from.language_code;
@@ -235,21 +187,6 @@ bot.start((ctx) => {
   
   ctx.reply('Hello')
 })
-
-bot.help((ctx) => ctx.reply('Help message'))
-
-bot.on('message', (ctx) => {
-  stepCheck(ctx);
-
-  var msg = makeMessage(ctx);
-  ctx.telegram.sendMessage(ctx.from.id, msg, Extra.HTML().markup(keyboard))
-  })
-
-
-
-
-
-bot.action('delete', ({ deleteMessage }) => deleteMessage())
 
 bot.action('id',(ctx) => {
   ctx.reply("Please input EOS account. You can check your account with EOS public key on http://eosflare.io .");
@@ -273,9 +210,7 @@ function makePriceMessage(res){
  msg += "Provided by " + res[1].exchange;
  //diff =  res[0].krw - res[1].krw;
  //msg += "Market difference : " + diff + "KRW";
- return msg;
-
- 
+ return msg; 
 }
 
 bot.action('price',(ctx) => {
@@ -291,8 +226,6 @@ bot.action('price',(ctx) => {
      db.close();
     });
    });
-
-
 });
 
 bot.action('balance',(ctx) => {
@@ -353,175 +286,8 @@ bot.action('balance',(ctx) => {
  }); //end of first load data
 
 //console.log('currency balance', balance);
-
- 
-
   ctx.session.step = 3;
 }); //end of bot action
-
-bot.action('ether',(ctx) => {
-  ctx.reply("input Ethereum Wallet Address please");
-  ctx.session.step = 3;
-});
-
-bot.action('confirm',(ctx) => {
-  //ctx.reply("input bitshare ID please");
-  //ctx.session.step = 1;
-  //DB Transaction processing
-  setEosBalance(ctx)
-  if(checkData(ctx) == true){
-
-                    saveData(ctx);
-
-        //saveData(ctx, -1);
-    var msg;
-    msg = "Completed.";
-    msg += "\n";
-    msg += "Airdrop will be done in a few days.";
-    msg += "\n";
-    msg += "Please use this referal link";
-    msg += "\n";
-    msg += "https://t.me/eoscafebot?start=";
-    msg += ctx.session.bts;
-    ctx.reply(msg);
-
-
-  }
-  else{
-    ctx.reply("Please input all data");
-  }
-});
-
-    
-
-const superWizard = new WizardScene('super-wizard',
-  (ctx) => {
-
-    
-
-  ctx.session.step = 0;
-    ctx.reply('1단계', Markup.inlineKeyboard([
-      Markup.urlButton('카톡 오픈챗에 가입', 'https://open.kakao.com/o/gj8CwMH'),
-      Markup.callbackButton('➡️ 다음', 'next')
-    ]).extra())
-  
-
-  
-    return ctx.wizard.next()
-  },
-  //stepHandler,
-  (ctx) => {
-  /*
-      //check korean or not. If not, then just return to step 1
-  if(ctx.message.from.language_code != "undefined")
-  if(ctx.message.from.language_code != "ko-KR"){
-    console.log("not Korean case");
-    ctx.reply("Only Korean can apply the airdrop, please wait for your country turn");
-    return ctx.scene.leave()
-  }
-  
-  
-  if(ctx.message.from.callback_query.from.language_code != "undefined")
-  if(ctx.message.from.callback_query.from.language_code != "ko_KR"){
-    console.log("not Korean case");
-    ctx.reply("Only Korean can apply the airdrop, please wait for your country turn");
-    return ctx.scene.leave()
-  }
-  */
-
-  
-    ctx.reply('2단계', Markup.inlineKeyboard([
-      Markup.urlButton('네이버카페 가입', 'http://cafe.naver.com/eoscafekorea'),
-      Markup.callbackButton('➡️ 다음', 'next')
-    ]).extra())
-    return ctx.wizard.next()
-  },
-  (ctx) => {
-    ctx.reply('3단계', Markup.inlineKeyboard([
-      Markup.urlButton('디스코드에 가입', 'https://discord.gg/BHEDGvx'),
-      Markup.callbackButton('➡️ 다음', 'next')
-    ]).extra())
-    return ctx.wizard.next()
-  },
-    (ctx) => {
-    ctx.reply('4단계 : email을 입력해주세요.')
-    return ctx.wizard.next()
-  },        
-      (ctx) => {
-  ctx.session.email = ctx.message.text;
-
-    ctx.reply('5단계 : Bitshare id를 입력해주세요.')
-
-    return ctx.wizard.next()
-  },
-        (ctx) => {
-  ctx.session.bts = ctx.message.text;
-    ctx.reply('6단계 : 이더리움 지갑 주소를 알려주세요.')
-
-    return ctx.wizard.next()
-  },         
-          (ctx) => {
-      ctx.session.etw = ctx.message.text;
-    ctx.reply('7단계 : 네이버 ID를 알려주세요.')
-
-    return ctx.wizard.next()
-},
-            (ctx) => {
-  ctx.session.ncafe = ctx.message.text;
-    ctx.reply('8단계 : 추천인 id를 알려주세요. 없으면 없음 이라고써주세요.')
-
-    return ctx.wizard.next()
-  },                                          
-  (ctx) => {
-    //console.log("birshare id", ctx.message.text);
-  //save ethereum wallet address
-    ctx.session.refer = ctx.message.text;
-  
-  
-   finalResult = "\n"  
-  finalResult += "입력하신 정보를 잘 확인해 주세요.";
-      finalResult += "\n"  
-  finalResult += "Your Email Address :"
-  finalResult += ctx.session.email;
-    finalResult += "\n"  
-  finalResult += "Your Bitshare ID :"
-  finalResult += ctx.session.bts
-    finalResult += "\n"  
-  finalResult += "Your Ethereum Wallet :"
-  finalResult += ctx.session.etw
-  finalResult += "\n"  
-      finalResult += "Your Naver ID :"
-  finalResult += ctx.session.ncafe
-  finalResult += "\n"  
-      finalResult += "Your referer :"
-  finalResult += ctx.session.refer
-  
-  ctx.reply("마지막 단계" + "\n" + finalResult + "\n"+ "\n"+ "몇일안에 에어드랍이 완료 됩니다.");
-/*
-      ctx.reply('Final step' + finalResult + "Airdrop will be done in a few day", Markup.inlineKeyboard([
-      //Markup.callbackButton('Go To First', 'first'),
-      Markup.callbackButton('Confirm', 'confirm') //this trigger first step of wizard, so removed.
-    ]).extra())
-    */
-  /* this prevent language detection so it goes to return state
-  if(ctx.data == "first")
-     return ctx.wizard.selectStep(ctx.session.step);
-     */
-
-
-    //ctx.reply('Done' + finalResult + "Airdrop will be done in a few day");
-    console.log(ctx.session.etw, ctx.session.bts, ctx.session.email);
-  
-  
-  
-    return ctx.scene.leave()
-  //This makes gurbage data and undefined issues
-  //return;
-  }
-)
-
-
-
 
 
 
@@ -531,98 +297,6 @@ bot.telegram.getMe().then((bot_informations) => {
     console.log("Server has initialized bot nickname. Nick: "+bot_informations.username);
 });
 
-// Command example, pretty easy. Each callback passes as parameter the context.
-// Context data includes message info, timestamp, etc; check the official documentation or print ctx.
-
-//bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
-
-
-//this did not work I think this need registration.
-bot.on('/ddd', msg => {
-
-    const id = msg.from.id;
- console.log("Bot On is called");
-
-    // Ask user name
-    return bot.sendMessage(id, 'What is your email?', {ask: 'email'});
-
-});
-
-bot.command('custom', ({ reply }) => {
-  //const stage = new Stage([superWizard], { default: 'super-wizard' })
-  //bot.use(stage.middleware())
-  return reply('Custom buttons keyboard', Markup
-    .keyboard([
-      ['🔍 Search', '😎 Popular'], // Row1 with 2 buttons
-      ['☸ Setting', '📞 Feedback'], // Row2 with 2 buttons
-      ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 buttons
-    ])
-    .oneTime()
-    .resize()
-    .extra()
-  )
-})
-
-// Hears, instead of command, check if the given word or regexp is CONTAINED in user input and not necessarly at beginning.
-bot.hears('ymca', (ctx) => {
-    console.log("saving session");
-    ctx.session.date = new Date();
-    ctx.reply("*sing* It's fun to stay at the Y.M.C.A.!")});
-
-bot.hears(/torino/i, (ctx) => { 
-    
-    ctx.reply(`Someone said Torino!, ${ctx.session.date}?`)});
-
-// Inline query support (@yourbot query). Can be used anywhere, even in groups. It works just like @gif bot.
-bot.on('inline_query', ctx => {
-    let query = ctx.update.inline_query.query;  // If you analyze the context structure, query field contains our query.
-    if(query.startsWith("/")){  // If user input is @yourbot /command
-        if(query.startsWith("/audio_src")){ // If user input is @yourbot /audio_src
-            // In this case we answer with a list of ogg voice data.
-            // It will be shown as a tooltip. You can add more than 1 element in this JSON array. Check API usage "InlineResultVoice".
-            return ctx.answerInlineQuery([
-                {
-                    type: 'voice',  // It's a voice file.
-                    id: ctx.update.inline_query.id,    // We reflect the same ID of the request back.
-                    title: 'Send audio file sample.ogg',    // Message appearing in tooltip.
-                    voice_url: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg',
-                    voice_duration: 16, // We can specify optionally the length in seconds.
-                    caption: '[BOT] Audio file sample.ogg!' // What appears after you send voice file.
-                }
-            ]);
-        }
-    }else{  // If user input is @yourbot name
-        let name_target = query;    // Let's assume the query is actually the name.
-        let message_length = name_target.length;    // Name length. We want to ensure it's > 0.
-        if(message_length > 0){
-            let full_message;
-            let dice=Math.floor(Math.random()*8)+1; // Let's throw a dice for a random message. (1, 8)
-            switch(dice){
-                case 1: full_message = "IMHO, "+name_target+" sucks."; break;
-                case 2: full_message = "IMHO, "+name_target+" is awesome"; break;
-                case 3: full_message = name_target+" is not a nice people for me..."; break;
-                case 4: full_message = name_target+" for me you are c- Eh! You wanted!"; break;
-                case 5: full_message = "Whoa! "+name_target+" is very cool!"; break;
-                case 6: full_message = "Grifondoro! No wait, "+name_target+" you're such a noob."; break;
-                case 7: full_message = "Sometimes I ask myself why people like "+name_target+" dress up and walk around like that..."; break;
-                case 8: full_message = "Watch him! Watch! "+name_target+" is so ugly!"; break;
-            }
-            // Let's return a single tooltip, not cached (In order to always change random value).
-            return ctx.answerInlineQuery([{
-                type: 'article',
-                id: ctx.update.inline_query.id, 
-                title: 'You have inserted: '+name_target, 
-                description: 'What does '+bot.options.username+' thinks about '+name_target+'?',
-                input_message_content: {message_text: full_message}
-            }], {cache_time: 0});
-        }
-    }
-})
-//const stage = new Stage([superWizard], { default: 'super-wizard' })
-
-// // Register session middleware
-
-//bot.use(stage.middleware())
 
 
 // Start bot polling in order to not terminate Node.js application.
