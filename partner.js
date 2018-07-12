0dd259be5c21801576fca5c56c09a7e089d3c523
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-12 11:40:58 +0900

    Update partner.js
    correction of misspelled.

--- partner.js
+++ partner.js
@@ -1,6 +1,6 @@
 module.exports.makePartnerMessage = function(){
   
-  let msg = "Developed in partnetship with Tokenbank,Aisa's largest ICO "; 
+  let msg = "Developed in partnership with Tokenbank, Asia's largest ICO "; 
   msg += "investment platform that links cryptocurrency investors with verified ICO projects.";
   msg += "\n" + "\n";
   msg += "www.tokenbank.co.kr";
