c66c42731f72962ab67d1b51d91f91943ac3c1fe
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-12 09:32:45 +0900

    Update partner.js

--- partner.js
+++ partner.js
@@ -1,8 +1,8 @@
 module.exports.makePartnerMessage = function(){
   
-  let msg = "Developed in partnetship with Tokenbank,Aisa's largest ICO"; 
+  let msg = "Developed in partnetship with Tokenbank,Aisa's largest ICO "; 
   msg += "investment platform that links cryptocurrency investors with verified ICO projects.";
-  msg += "\n";
+  msg += "\n" + "\n";
   msg += "www.tokenbank.co.kr";
 
   return msg;
