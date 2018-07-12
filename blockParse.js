d8a9492f54cf0c33ca598802afafcc6289795c1b
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-13 08:57:37 +0900

    Update blockParse.js

--- blockParse.js
+++ blockParse.js
@@ -1,1 +1,27 @@
+const Eos = require('eosjs')
 
+eosConfig = {
+ httpEndpoint: "https://mainnet.eoscalgary.io"
+}
+
+eos = Eos(eosConfig)
+
+const blockIdx = 5498555;
+function testBlock(){
+  eos.getBlock(blockIdx).then(result => {
+    const trx = result.transactions[0].trx.transaction;
+    const type = trx.actions[0].name;
+    const data = trx.actions[0].data;
+    //check data feasibility
+    console.log(data);
+    //check account field test
+    
+    if(("authorization" in result) == 0)
+      console.log("authorization field exist");
+    if(("authorization" in trx) == 0)
+      console.log("authorization field exist");
+    
+
+  
+  
+}
