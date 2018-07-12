16891a3291ff6202e0363b6274d8d8a8327af9ac
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-12 09:23:07 +0900

    Update partner.js

--- partner.js
+++ partner.js
@@ -8,7 +8,7 @@ module.exports.makePartnerMessage = function(){
                               verified ICO projects.
                               
                               www.tokenbank.co.kr
-  ';
+  '
 
   return msg;
 }
