6f21fbf78bd88059ba337981ebcb6fa9fe84ee27
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-12 12:39:42 +0900

    Update eos.js

--- eos.js
+++ eos.js
@@ -19,6 +19,7 @@ function getLatestBlock(){
  eos.getInfo({}).then(result => {
   //console.log(result);
   startIndex = result.last_irreversible_block_num;
+  console.log("getinfo block", startIndex
   if(previousReadBlock < startIndex){
    idx = startIndex;
    //read block
