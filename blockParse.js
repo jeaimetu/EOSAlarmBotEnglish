240aee094db9cf78f4239eb0cae25c9e56d34b06
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-13 09:39:12 +0900

    Update blockParse.js

--- blockParse.js
+++ blockParse.js
@@ -1,31 +1,25 @@
-const Eos = require('eosjs')
-
-eosConfig = {
- httpEndpoint: "https://mainnet.eoscalgary.io"
-}
-
-eos = Eos(eosConfig)
-
-const blockIdx = 5498555;
-function testBlock(){
-  eos.getBlock(blockIdx).then(result => {
-    const trx = result.transactions[0].trx.transaction;
-    const type = trx.actions[0].name;
-    const data = trx.actions[0].data;
-    //check data feasibility
-    console.log(data);
-    //check account field test
-    
-    if(("authorization" in result) == 0)
-      console.log("authorization field exist");
-    if(("authorization" in trx) == 0)
-      console.log("authorization field exist");
-  });
-    
-
-  
-  
+function getAccountInfo(data){
+ let account = null;
+ 
+ if(("to" in data) == 0)
+  account = data.to;
+ if(("from" in data) == 0)
+  account = data.from;
+ if(("account" in data) == 0)
+  account = data.account;
+ if(("owner" in data) == 0)
+  account = data.owner;
+ if(("voter" in data) == 0)
+  account = data.voter;
+ if(("receiver" in data) == 0)
+  account = data.receiver;
+ if(("poster" in data) == 0)
+  account = data.poster;
+ 
+ rturn account; 
 }
 
 
-testBlock();
+//Exporting functions
+modulee.exports.getAccountInfo = getAccountInfo;
+
