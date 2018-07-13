da21fce518f1c19cd7ff8247ff9e397d07615cd2
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-13 12:21:27 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -425,6 +425,7 @@ module.exports.sendAlarm = function(account, msg){
    }else{
      //send message
     for(i = 0;i < result.length; i++){
+     console.log("try to send message to ", result[i].chatid);
      bot.telegram.sendMessage(result[i].chatid, msg).catch((error) => {
       console.log(error);
       console.log("remove user with chatid ", result[i].chatid);
