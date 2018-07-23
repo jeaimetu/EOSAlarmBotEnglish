3c1596dfc68215f74a5b50412054d2b9fa40e6c3
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-23 11:58:28 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -416,10 +416,11 @@ bot.start((ctx) => {
   ctx.session.language = ctx.message.from.language_code
  
   reset(ctx)
-  let msg = makeMessage(ctx)
+
   
   loadData(ctx, id => {
    ctx.session.id = id;
+	    let msg = makeMessage(ctx)
    ctx.telegram.sendMessage(ctx.from.id, msg, Extra.HTML().markup(keyboard))
   })
   
